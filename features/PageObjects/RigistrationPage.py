bc1cc661e75eb7bf1d4d2e93a7e82781c1bb5383
Kiran Kumar <saikiran7008@gmail.com>
2022-08-19 16:00:06 +0530

    updates

---
+++ features/PageObjects/RigistrationPage.py
@@ -0,0 +1,31 @@
+from Utilities.Configreader import config_reader
+from features.PageObjects.BasePage import BasePage
+
+
+class RegistrationPage(BasePage):
+    def __init__(self,driver):
+        super().__init__(driver)
+
+    def setName(self,name):
+        self.driver.entry(config_reader("name_Xpath"),name)
+
+    def setphoneNum(self,phoneNum):
+        self.driver.entry(config_reader("name_Xpath"),phoneNum)
+
+    def setemail(self,email):
+        self.driver.entry(config_reader("name_Xpath"),email)
+
+    def setcity(self,city):
+        self.driver.entry(config_reader("name_Xpath"),city)
+
+    def setcountry(self,country):
+        self.driver.select(config_reader("name_Xpath"),country)
+
+    def setusername(self,username):
+        self.driver.entry(config_reader("name_Xpath"),username)
+
+    def setpassword(self,password):
+        self.driver.entry(config_reader("name_Xpath"),password)
+
+    def submit(self):
+        self.driver.click(config_reader("name_Xpath"))
--- features/steps/registration_steps.py
+++ features/steps/registration_steps.py
@@ -1,30 +1,34 @@
 from behave import *
 
 
+from features.PageObjects.RigistrationPage import RegistrationPage as RP
+
+
 @given(u'I navigate to the URL qa.way2automation.com')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Given I navigate to the URL qa.way2automation.com')
 
 
 @when(u'I enter name as "<name>"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When I enter name as "<name>"')
+def step_impl(context,name):
+    RP.setName(name)
 
 
 @then(u'I enter phonenum as "<phonenum>"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I enter phonenum as "<phonenum>"')
+def step_impl(context,phonenum):
+    RP.setphoneNum(phonenum)
 
 
 @then(u'I enter email as "<email>"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I enter email as "<email>"')
+def step_impl(context,email):
+    RP.setemail(email)
 
 
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I enter username as "<username>"')
+@then(u'I enter password as "<username>"')
+def step_impl(context,username):
+    RP.setusername()
 
 
 @then(u'I enter password as "<password>"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I enter password as "<password>"')
+def step_impl(context,password):
+    RP.setpassword(password)
